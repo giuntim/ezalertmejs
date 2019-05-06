074f4e8405fc2b32ed7c212badf6d469c063e012
giuntim <mgiunti@gmail.com>
2019-05-06 23:11:25 +0200

    Improved sample and docs

--- ezalertme.test.js
+++ ezalertme.test.js
@@ -3,7 +3,7 @@
 const ezalertme = require('./ezalertme.js')
 
 test('Send an alert via https://EZAlert.me', () => {
-  return ezalertme.sendAlert('EZalert.me JS test').then( data => {
+  return ezalertme.sendAlert('EZalert.me JS test').then(data => {
     console.log(data)
     expect(data.status).toBeDefined()
   })
--- sample.js
+++ sample.js
@@ -1,8 +1,16 @@
 const ezalertme = require('./ezalertme')
 
-// If you need you can set your EZAlert.me apikey here
+// Usually you set EZAlert.me apikey as env variable with
+// set EZALERTMEKEY='Your API key'
+// But if you need you can set your it in the code with
 // ezalertme.token='My EZALert.me API key'
 
 ezalertme.sendAlert('Houston, we have a problem!').then(rc => {
   console.log(rc)
+  // If I need I can ckeck if returned object is an error (satus==ERROR) or not (status==OK)
+  if (rc && rc.status === 'OK') {
+    console.log('Ok: Alert Sent')
+  } else {
+    console.log('Error: alert not sent')
+  }
 })
